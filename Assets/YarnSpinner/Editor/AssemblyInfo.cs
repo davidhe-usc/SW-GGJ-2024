45b14eb3d6fdaaa9737faed33c7daf1129ceaa14
Rachel Geng <rgeng@usc.edu>
2024-01-25 18:24:08 -0800

    Importing YarnSpinner Package

---
+++ Assets/YarnSpinner/Editor/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("2.4.1.1")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
+[assembly: AssemblyInformationalVersion("2.4.1-assetstore.1+4.Branch.assetstore.Sha.e0683e1c79e744a269cf4bd8bcea7aac2f7517e8")]
+
+[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
