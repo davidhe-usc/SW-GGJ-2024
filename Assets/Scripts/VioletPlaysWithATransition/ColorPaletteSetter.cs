14697e9f4695d6ffd7bc04bfbc38ab73c388ce1a
violetfairy <44590656+violetfairy@users.noreply.github.com>
2024-01-27 22:29:25 -0800

    Transition Test
    I added some assets for a fun scene transition and messed around with it a bit in a couple scenes.

---
+++ Assets/Scripts/VioletPlaysWithATransition/ColorPaletteSetter.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPaletteSetter : MonoBehaviour
+{
+    [SerializeField]
+    Image[] lightImages;
+    [SerializeField]
+    Image[] darkImages;
+    [SerializeField]
+    Color[] lightColors;
+    [SerializeField]
+    Color[] darkColors;
+    [SerializeField]
+    int startingPalette;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startingPalette < lightColors.Length)
+        {
+            SetPalette(startingPalette);
+        } else
+        {
+            SetRandomPalette();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPalette(int paletteID)
+    {
+        for (int i = 0; i < lightImages.Length; i++)
+        {
+            lightImages[i].color = lightColors[paletteID];
+        }
+        for (int i = 0; i < darkImages.Length; i++)
+        {
+            darkImages[i].color = darkColors[paletteID];
+        }
+    }
+
+    public void SetRandomPalette()
+    {
+        SetPalette(Random.Range(0, lightColors.Length)); 
+    }
+}
